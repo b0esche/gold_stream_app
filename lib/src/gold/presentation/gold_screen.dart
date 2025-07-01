ecf8b38147d8bf4b40585994a71df9a525c1811e
Leon Bösche <leonboesche1999@gmail.com>
2025-07-01 10:24:25 +0200

    tasksheet 6.1.2

--- lib/src/gold/presentation/gold_screen.dart
+++ lib/src/gold/presentation/gold_screen.dart
@@ -1,15 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:gold_stream_app/src/gold/data/fake_gold_api.dart';
 import 'package:gold_stream_app/src/gold/presentation/widgets/gold_header.dart';
 import 'package:intl/intl.dart';
 
-class GoldScreen extends StatelessWidget {
+class GoldScreen extends StatefulWidget {
   const GoldScreen({super.key});
+
+  @override
+  State<GoldScreen> createState() => _GoldScreenState();
+}
+
+class _GoldScreenState extends State<GoldScreen> {
+  late final Stream<double> goldStream;
+  @override
+  void initState() {
+    super.initState();
+    goldStream = getGoldPriceStream();
+  }
+
   @override
   Widget build(BuildContext context) {
-    /// Platzhalter für den Goldpreis
-    /// soll durch den Stream `getGoldPriceStream()` ersetzt werden
-    double goldPrice = 69.22;
-
     return SafeArea(
       child: Scaffold(
         body: Padding(
@@ -24,13 +34,29 @@ class GoldScreen extends StatelessWidget {
                 style: Theme.of(context).textTheme.headlineMedium,
               ),
               SizedBox(height: 20),
-              // TODO: Verwende einen StreamBuilder, um den Goldpreis live anzuzeigen
-              // statt des konstanten Platzhalters
-              Text(
-                NumberFormat.simpleCurrency(locale: 'de_DE').format(goldPrice),
-                style: Theme.of(context).textTheme.headlineLarge!.copyWith(
-                  color: Theme.of(context).colorScheme.primary,
-                ),
+              StreamBuilder<double>(
+                stream: goldStream,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    final price = snapshot.data!;
+                    return Text(
+                      NumberFormat.simpleCurrency(
+                        locale: 'de_DE',
+                      ).format(price),
+                      style: Theme.of(context).textTheme.headlineLarge!
+                          .copyWith(
+                            color: Theme.of(context).colorScheme.primary,
+                          ),
+                    );
+                  } else if (snapshot.connectionState ==
+                      ConnectionState.waiting) {
+                    return const CircularProgressIndicator();
+                  } else if (snapshot.hasError) {
+                    return const Text("Fehler beim Laden");
+                  } else {
+                    return const Text("Keine Daten verfügbar");
+                  }
+                },
               ),
             ],
           ),
